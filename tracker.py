2575d13f50abc3f4938e5362b7f5680bc69ad59c
Erkan <erkancekic_01@hotmail.com>
2024-06-02 18:44:09 +0300

    Config dosyası yüklendi

--- tracker.py
+++ tracker.py
@@ -5,3 +5,13 @@ import sys
 import argparse
 from datetime import datetime
 import matplotlib.pyplot as plt
+
+try:
+    with open("url.config", "r") as f:
+        api_urls = json.load(f)
+except FileNotFoundError:
+    print("HATA: Konfigürasyon dosyası olan 'url.config' bulunamadı.")
+    sys.exit(1)
+except json.JSONDecodeError as e:
+    print(f"HATA:  JSON kod çözme hatası 'url.config': {e}")
+    sys.exit(1)
